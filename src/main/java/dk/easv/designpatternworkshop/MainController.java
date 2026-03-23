224cb9b73254c2091d58ae8c8ba580b407e060df
Just A Bee <rafal.jan.laskowski@gmail.com>
2026-03-23 12:09:17 +0100

    Update MainController.java

--- src/main/java/dk/easv/designpatternworkshop/MainController.java
+++ src/main/java/dk/easv/designpatternworkshop/MainController.java
@@ -5,14 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
     public void btnReload(ActionEvent actionEvent) {
     }
 
