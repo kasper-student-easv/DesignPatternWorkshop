4dd58254589f30dfe1fa155f7da46b1bc94556a9
jonlinnet28 <jonlinnet28@gmail.com>
2026-03-23 11:08:56 +0100

    Button in main controller

--- src/main/java/dk/easv/designpatternworkshop/MainController.java
+++ src/main/java/dk/easv/designpatternworkshop/MainController.java
@@ -1,5 +1,6 @@
 package dk.easv.designpatternworkshop;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,26 @@ public class MainController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void btnReload(ActionEvent actionEvent) {
+    }
+
+    public void btnConnect(ActionEvent actionEvent) {
+    }
+
+    public void btnDisconnect(ActionEvent actionEvent) {
+    }
+
+    public void btnSendConfig(ActionEvent actionEvent) {
+    }
+
+    public void btnReadResult(ActionEvent actionEvent) {
+    }
+
+    public void btnResetResult(ActionEvent actionEvent) {
+    }
+
+    public void btnLoadTemplate(ActionEvent actionEvent) {
     }
 }
